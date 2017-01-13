588c6fab2c8c36d296088207746f137fc49936f6
Ubuntu <ubuntu@ip-172-31-46-190.us-west-2.compute.internal>
2017-01-13 23:21:00 +0000

    edited max businesses and max photo values

--- start_atitan.sh
+++ start_atitan.sh
@@ -2,5 +2,9 @@
 cd /home/ubuntu/atitan
 virtualenv BASELINE
 . BASELINE/bin/activate
-pip install Flask
+#pip install Flask
+python setup.py install
+#pip install requests
+#pip install grequests
+pip install -r requirements.txt
 python atitan.py
